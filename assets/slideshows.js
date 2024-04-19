a09892df0ea148eda018d88b10d45dc042847ecb
TheSam12 <osbel_torres@hotmail.com>
2024-04-19 17:23:31 -0400

    Update slideshows.js

--- assets/slideshows.js
+++ assets/slideshows.js
@@ -63,6 +63,7 @@ document.addEventListener('alpine:init', () => {
           interval: this.autoplayInterval,
           autoplay: this.autoplay,
           drag: !this.autoplay,
+          perPage: this.slidesToShow || 1,
         };
 
         if (this.mode === 'carousel') {
