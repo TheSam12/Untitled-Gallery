55a371d3b81083562fdf1045f8803ee6d5e5d49a
TheSam12 <osbel_torres@hotmail.com>
2024-04-20 18:40:20 -0400

    Update slideshows.js

--- assets/slideshows.js
+++ assets/slideshows.js
@@ -55,7 +55,7 @@ document.addEventListener('alpine:init', () => {
       initSplide() {
         const options = {
           type: this.loop? 'loop' : 'carousel',
-          perPage: 4,
+          perPage: this.perPage,
           arrows: true,
           pagination: true,
           rewind: true,
