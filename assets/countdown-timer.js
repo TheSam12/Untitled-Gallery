f0386ef5616510112b7a26ac1e8e9352c7a63d4d
TheSam12 <osbel_torres@hotmail.com>
2024-05-15 18:00:42 -0400

    test

--- assets/countdown-timer.js
+++ assets/countdown-timer.js
@@ -15,7 +15,7 @@ function updateCountdown(date, element) {
   const diffMins = Math.floor((diffMs % 3600000) / 60000); // minutes
   const diffSecs = Math.floor((diffMs % 60000) / 1000); // seconds
 
-  element.innerHTML = `Starts in ${diffDays}d ${diffHrs}h ${diffMins}m ${diffSecs}s`;
+  element.innerHTML = `${diffDays}d ${diffHrs}h ${diffMins}m ${diffSecs}s`;
 
   if (diffMs < 0) {
     clearInterval(intervalId);
@@ -40,7 +40,7 @@ function updateCountdownCloses(date, element) {
   const diffMins = Math.floor((diffMs % 3600000) / 60000); // minutes
   const diffSecs = Math.floor((diffMs % 60000) / 1000); // seconds
 
-  element.innerHTML = `Closes in ${diffDays}d ${diffHrs}h ${diffMins}m ${diffSecs}s`;
+  element.innerHTML = `${diffDays}d ${diffHrs}h ${diffMins}m ${diffSecs}s`;
 
   if (diffMs < 0) {
     clearInterval(intervalId);
